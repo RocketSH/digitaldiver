238f5bed9b5977b1315699b282d28d5a159d30ae
digitaldiver <1119han@gmail.com>
2022-10-13 02:02:16 +0200

    add links style

--- components/BlogItem/index.tsx
+++ components/BlogItem/index.tsx
@@ -1,18 +1,28 @@
 import React from "react";
+import Link from "next/link";
 import { formatDate } from "@lib/dateHelpers";
 import { PostData } from "@interfaces";
 import styles from "./BlogItem.module.sass";
+import path from "path";
 
 interface Props {
   article: PostData;
 }
 
 const BlogItem: React.FC<Props> = ({ article }) => {
+  const href = `/blog/${article.slug}`
   return (
     <article className={styles.article}>
-      <h3>{article.title}</h3>
+      <Link href={href}>
+        <a>
+          <h3>{article.title}</h3>
+        </a>
+      </Link>
       <time>{formatDate(article.date)}</time>
       <p>{article.description}</p>
+      <Link href={href}>
+        <a className={styles.readMore}>Read more...</a>
+      </Link>
     </article>
   );
 };
