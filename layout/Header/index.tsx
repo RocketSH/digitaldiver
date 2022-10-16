da8dfa3417f14ddcabfcfb60bcc6c035283fb73f
digitaldiver <1119han@gmail.com>
2022-10-16 18:15:48 +0200

    add navbar menu

--- layout/Header/index.tsx
+++ layout/Header/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import styles from "./Header.module.sass"
+import styles from "./Header.module.sass";
 
 const Header: React.FC = () => {
   return (
@@ -10,6 +10,20 @@ const Header: React.FC = () => {
           <h1>digitaldiver</h1>
         </a>
       </Link>
+      <div className={styles.nav}>
+        <div className={styles.items}>
+        <Link href="/">Home</Link>
+        </div>
+        <div className={styles.items}>
+        <Link href="/blog">Blog</Link>
+        </div>
+        <div className={styles.items}>
+        <Link href="/contact">Contact</Link>
+        </div>
+        <div className={styles.items}>
+        <Link href="/resume.pdf" className={styles.resume}>Resume</Link>
+        </div>
+      </div>
     </header>
   );
 };
--- layout/Hero/index.tsx
+++ layout/Hero/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./Hero.module.sass";
 
 const Hero: React.FC = () => {
-return (
+  return (
     <section className={styles.hero}>
       <div className={styles.wrapper}>
-        <h1 className={styles.subtitle}>Hi there, I am</h1>
+        <h1 className={styles.subtitle}>Hi there, I am &#128512;</h1>
         <h1 className={styles.title}>ShuHan 舒涵</h1>
-        <div />
+        <p>I am a Ruby on Rails developer based in Warsaw, Poland.</p>
       </div>
     </section>
   );
